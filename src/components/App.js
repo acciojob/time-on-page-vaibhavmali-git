c679b3e3f09694134a59e15e3ca3fa18590c3d1b
VaibhavMali001 <malivaibhav.7620@gmail.com>
2024-04-16 13:33:49 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,22 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useEffect, useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setSeconds((prevSec) => prevSec + 1);
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <p>You've been on this page for {seconds} seconds.</p>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
